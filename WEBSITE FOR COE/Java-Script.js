87ec8b94a2e416d8aeb4d14a796931670376faa0
Tamzeyy <jerichotamoro1902@gmail.com>
2024-10-06 18:41:32 +0800

    Add files via upload

---
+++ WEBSITE FOR COE/Java-Script.js
@@ -0,0 +1,9 @@
+// Redirect the logo to the home page
+document.getElementById('logo').addEventListener('click', function() {
+    window.location.href = 'HOME.html';
+});
+
+// Redirect Home link to the home page
+document.getElementById('home-link').addEventListener('click', function() {
+    window.location.href = 'HOME.html';
+});
